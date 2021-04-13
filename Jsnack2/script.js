f917bf73729bf75362d0754adf6025eebec0bf34
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-13 17:08:07 +0200

    jsnack2

--- Jsnack2/script.js
+++ Jsnack2/script.js
@@ -1,16 +1,15 @@
 // Inserisci un numero, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
-var numero = prompt("Inserisci un numero"); // chiedo all'utente di inserire un numero
+var numero = parseInt(prompt("Inserisci un numero")); // chiedo all'utente di inserire un numero
 // var numero = 11;
 console.log("Hai inserito il numero " + numero)
 console.log("----")
 
-var incremento = parseInt("1");
-
+var incremento = parseInt("1"); // var incremento di 1 
 
 if(numero%2 === 0){
   console.log("Hai inserito il numero " + numero) // se pari stampo il numero
-} else { // se dispari stampo il numero successivo 
-  console.log(parseInt(numero) + parseInt(incremento))
+} else { // se dispari stampo il numero successivo aggiungnedo la var incremento
+  console.log(parseInt(numero) + incremento) 
 }
